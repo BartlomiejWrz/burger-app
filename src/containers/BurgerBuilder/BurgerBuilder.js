e9b67718508810ace772ceb3a8041fd9d174f96e
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-02-03 19:11:48 +0100

    builded burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
 	render() {
 		return (
 			<div>
+				<Burger />
 				<div>Burger</div>
 				<div>Build Control</div>
 			</div>
