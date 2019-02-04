00d0d7f41b2ec48ef247035ba93b6d82d42f5f95
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-02-04 17:11:39 +0100

    building burger cd.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+	//consttructor (props) {
+	//super (props);
+	//	this.state ={...};
+	//}
+
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				<Burger />
-				<div>Burger</div>
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Control</div>
 			</div>
 		);
