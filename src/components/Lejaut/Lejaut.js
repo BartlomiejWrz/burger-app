f97077e2bad583345e20dad11f7a44e8ce7acb8d
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-02-08 19:43:53 +0100

    mobile rwd

--- src/components/Lejaut/Lejaut.js
+++ src/components/Lejaut/Lejaut.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import styles from './Lejaut.module.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
 	<div>
 		<Toolbar />
+		<SideDrawer />
 		<main className={styles.Content}>
 			{props.children}
 		</main>
