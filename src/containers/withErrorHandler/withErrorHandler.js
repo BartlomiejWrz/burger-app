164b359771818171b77e11205801cb39f61dc13d
Bartlomiej Wrzesinski <bartlomiej18@op.pl>
2019-02-16 20:23:40 +0100

    improved app

--- src/containers/withErrorHandler/withErrorHandler.js
+++ src/containers/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,19 @@ const withErrorHandler = (WrappedComponent, anxios) => {
 		}
 
 		componentWillMount() {
-			axios.interceptors.request.use(req => {
+			this.reqInterceptor = axios.interceptors.request.use(req => {
 				this.setState({ error: null });
 				return req;
 			});
-			axios.interceptors.response.use(res => res, null, error => {
+			this.resInterceptor = axios.interceptors.response.use(res => res, null, error => {
 				this.setState({ error: error });
 			});
+		}
+
+		componentWillUnmount() {
+			axios.interceptors.request.eject(this.reqInterceptor);
+			axios.interceptors.response.eject(this.resInterceptor);
+
 		}
 
 		errorConfirmedHandler() {
